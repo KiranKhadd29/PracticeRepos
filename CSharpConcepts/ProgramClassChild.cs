f6f399d3220bbc69e0e3b4ae526352b56376fdc7
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-08-25 09:01:05 +0530

    Access Modifier

---
+++ CSharpConcepts/ProgramClassChild.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    class ProgramClassChild : Program
+    {
+        static void Main()
+        {
+            ProgramClassChild pro = new ProgramClassChild();
+            //pro.test1();
+            pro.test2();
+            pro.test3();
+            pro.test4();
+            pro.test5();
+            Console.ReadKey();
+
+            /********** output **************
+             * Private - Child class ->Not Accessible
+             * public - Child class
+             * internal - Child class
+             * protected - Child class
+             * protected internal- Child class
+             * *******************************/
+
+        }
+    }
+
+    
+}
---
+++ CSharpConcepts/ProgramClassNonChild.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    class ProgramClassNonChild
+    {
+        void Main()
+        {
+            Program pro = new Program();
+            //pro.test1();
+            pro.test2();
+            pro.test3();
+            //pro.test4();
+            pro.test5();
+            Console.ReadKey();
+
+            /********** output **************
+             * Private - Non Child class ->Not Accessible
+             * public - Non Child class
+             * internal - Non  Child class
+             * protected - Non Child class ->Not Accessible
+             * protected internal- Non Child class
+             * *******************************/
+        }
+    }
+}
