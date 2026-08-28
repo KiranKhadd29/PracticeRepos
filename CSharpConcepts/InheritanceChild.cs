dcad458a9263aa99d06f456cbc2b40a6a4247421
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-08-28 09:45:10 +0530

    Inhertance concept

---
+++ CSharpConcepts/InheritanceChild.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    class InheritanceChild:InheritanceClass
+    {
+        InheritanceChild() : base ("Test") {}
+        void Test3()
+        {
+            Console.WriteLine("Test3 Excecuted from Child!!!");
+        }
+        public static void Main()
+        {
+            InheritanceChild child = new InheritanceChild();
+            child.Test1();
+            child.Test2();
+            child.Test3();
+            Console.ReadKey();
+        }
+        
+    }
+}
