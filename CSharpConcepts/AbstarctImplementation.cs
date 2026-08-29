0a9e13419bc06172db468e3ac85b13188f56c48d
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-08-29 10:10:11 +0530

    Abstract classes and methods

---
+++ CSharpConcepts/AbstarctImplementation.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    class AbstarctImplementation
+    {
+        public static void Main()
+        {
+            Circle cir = new Circle(12.5);
+            Console.WriteLine("Area of Circle = " + cir.GetArea());
+
+            Rectangle rect = new Rectangle(10.100, 15);
+            Console.WriteLine("Area of Rectangle = " + rect.GetArea());
+
+            Console.ReadKey();
+
+        }
+    }
+
+    abstract class Figure
+    {
+        public double Ht, Wd, Rd;
+        public const float Pi = 3.14f;
+
+        public abstract double GetArea();
+    }
+
+    class Circle : Figure
+    {
+        public Circle(Double Radius)
+        {
+            this.Rd = Radius;
+        }
+        public override double GetArea()
+        {
+            return Pi * Rd * Rd;
+        }
+        
+    }
+
+    class Rectangle : Figure
+    {
+        public Rectangle(Double Width, Double Height)
+        {
+            this.Wd = Width;
+            this.Ht = Height;
+        }
+        public override double GetArea()
+        {
+            return Wd * Ht;
+        }
+    }
+
+
+}
---
+++ CSharpConcepts/AbstractParent.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    abstract class AbstractParent
+    {
+        public int Add(int x, int y)
+        {
+            return x + y;
+        }
+        public int Sub(int x, int y)
+        {
+            return x - y;
+        }
+
+        public abstract int Mul(int x, int y);
+        public abstract int Div(int x, int y);
+    }
+
+    class AbstractChild : AbstractParent
+    {
+        public override int Mul(int x, int y)
+        {
+            return x*y;
+        }
+
+        public override int Div(int x, int y)
+        {
+            return x / y;
+        }
+
+        public static void Main()
+        {
+            AbstractChild abs = new AbstractChild();
+            Console.WriteLine("Addition :" + abs.Add(2, 2));
+            Console.WriteLine("Substarction :" + abs.Sub(2, 2));
+            Console.WriteLine("Multiplication :" + abs.Mul(2, 2));
+            Console.WriteLine("Division :" + abs.Div(2, 2));
+
+            Console.ReadKey();
+        }
+
+    }
+}
