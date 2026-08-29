5557f333cf9cd6e316df3ed7d5abd135d95aae1d
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-08-29 11:27:35 +0530

    Interface concept

---
+++ CSharpConcepts/Interface.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    interface ITestInterface1
+    {
+        int Add(int a, int b);
+    }
+
+    interface ITestInterface2 : ITestInterface1
+    {
+        int Sub(int a, int b);
+    }
+
+    class InterfaceImplementation : ITestInterface2
+    {
+        public int Add (int a, int b)
+        {
+            return a + b;
+        }
+
+        public int Sub(int a, int b)
+        {
+            return a - b;
+        }
+
+        public static void Main()
+        {
+            InterfaceImplementation iface = new InterfaceImplementation();
+            
+            int add = iface.Add(1, 2);
+            int sub = iface.Sub(1, 2);
+            Console.WriteLine("Addition = " + add);
+            Console.WriteLine("Substarction = " + sub);
+            Console.ReadKey();
+        }
+
+
+    }
+}
