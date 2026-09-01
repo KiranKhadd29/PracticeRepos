a1905eeefe700c73b7fd2e68ca2c0316184f0809
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-09-01 09:25:26 +0530

    Indexer example

---
+++ CSharpConcepts/Employee.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    public class Employee
+    {
+        int empCode;
+        string empName, dept, designation;
+        double salary;
+
+        public Employee(int EmpCode, string EmpName, string Department, string Designation, double Salary)
+        {
+            empCode = EmpCode;
+            empName = EmpName;
+            dept = Department;
+            designation = Designation;
+            salary = Salary;
+        }
+
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 1)
+                    return empCode;
+                if (index == 2)
+                    return empName;
+                if (index == 3)
+                    return dept;
+                if (index == 4)
+                    return designation;
+                if (index == 5)
+                    return salary;
+
+                return null;
+            }
+
+
+        }
+
+    }
+}
---
+++ CSharpConcepts/IndexerExample.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    class IndexerExample
+    {
+        public static void Main()
+        {
+            Employee emp = new Employee(1311, "Kiran K", "Delivery", "Developer", 25000.00);
+            Console.WriteLine("Employee Info:");
+            Console.WriteLine("Employee code: " + emp[1]);
+            Console.WriteLine("Employee Name: " + emp[2]);
+            Console.WriteLine("Employee Department: " + emp[3]);
+            Console.WriteLine("Employee Designation: " + emp[4]);
+            Console.WriteLine("Employee Salary: " + emp[5]);
+
+            Console.ReadKey();
+
+        }
+
+    }
+}
