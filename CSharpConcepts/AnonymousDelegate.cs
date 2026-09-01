67ef57da353217c86e7b126111d7219cabf7266d
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-09-01 19:25:35 +0530

    Delegates - Concept,Anonymous,Lambda,MultiCast and generic

---
+++ CSharpConcepts/AnonymousDelegate.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpConcepts
+{
+    public delegate string Greetings(string s);
+    class AnonymousDelegate
+    {
+        public static void Main()
+        {
+            Greetings obj = delegate (string name)
+            {
+                return "Hello," + name + " Good Morning!!!";
+            };
+
+            string str = obj.Invoke("Kiran");
+            Console.WriteLine(str);
+
+            Console.ReadKey();
+
+        }
+    }
+}
