cd3ec4988d3d164c94d650d1ef0ff7127ebc0bf2
KiranKhadd <kiran.khadd@prorigosoftware.com>
2026-09-05 12:31:10 +0530

    SOLID Principle

---
+++ SOLIDPrinciple/1_SingleResponsibilityPrinciple_SRP.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciple
+{
+    class SingleResponsibilityPrinciple_SRP: WrongApproch_Employee
+    {
+        public static void Main()
+        {
+            SingleResponsibilityPrinciple_SRP srp = new SingleResponsibilityPrinciple_SRP();
+            srp.Name = "Kiran Khadd";
+            srp.Salary = 25000;
+            Console.WriteLine("---- Wrong Approach ----");
+            srp.CalculateSalary();
+            srp.GenerateReport();
+
+            Console.WriteLine("---- Correct Approach ----");
+            SalaryCalculation sal = new SalaryCalculation();
+            sal._Name = "John K";
+            sal._Salary = 10000;
+            sal.Salary();
+
+            ReportGeneration rep = new ReportGeneration();
+            rep._Name = "Donald KT";
+            rep._Salary = 110000;
+            rep.Report();
+
+            Console.ReadLine();
+        }
+    }
+
+    //Violation of SRP
+    class WrongApproch_Employee
+    {
+        public string Name { get; set; }
+        public double Salary { get; set; }
+
+        public void CalculateSalary()
+        {
+            Console.WriteLine("{0} has credited {1} salary", Name, Salary);
+        }
+
+        public void GenerateReport()
+        {
+            Console.WriteLine("{0} has generated the report.", Name);
+        }
+    }
+
+    //Solution - SRP
+
+    class Correct__Employee
+    {
+        public string _Name { get; set; }
+        public double _Salary { get; set; }
+        
+    }
+
+    class SalaryCalculation : Correct__Employee
+    {
+        public void Salary()
+        {
+            Console.WriteLine("{0} has credited {1} salary", _Name, _Salary);
+        }
+    }
+
+    class ReportGeneration : Correct__Employee
+    {
+        public void Report()
+        {
+            Console.WriteLine("{0} has generated the report.", _Name);
+        }
+        
+    }
+}
---
+++ SOLIDPrinciple/3_LiskovSubstitutionPrinciple_LSP.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SOLIDPrinciple
+{
+    class LiskovSubstitutionPrinciple_LSP
+    {
+        public static void Main()
+        {
+            
+
+            Console.WriteLine("---- Wrong Approach ----");
+            WrongApproach_Bird bird = new WrongApproach_Bird();
+            bird.Fly("Parrot");
+            Orchid orc= new Orchid();
+            //orc.Fly("Orchid");//This is not implemented method
+
+            Console.WriteLine("---- Correct Approach ----");
+            FlyingBird par = new FlyingBird();
+            par.BirdName = "Parrot";
+            par.Fly();
+
+            NonFlyingBird orch = new NonFlyingBird();
+            orch.BirdName = "Orchids";
+            orch.NonFly();
+
+            Console.ReadLine();
+
+        }
+        
+    }
+
+    public class WrongApproach_Bird
+    {
+        public virtual void Fly(string BirdName)
+        {
+            Console.WriteLine("{0} can fly in air nicely.", BirdName);
+        }
+    }
+
+    public class Orchid : WrongApproach_Bird
+    {
+        public override void Fly(string BirdName)
+        {
+            throw new NotImplementedException($"{BirdName} can't fly.");
+        }
+    }
+
+    public abstract class CorrectApproach_Bird
+    {
+        public string BirdName { get; set; }
+
+    }
+
+    public class FlyingBird : CorrectApproach_Bird
+    {
+        
+        public virtual void Fly()
+        {
+            Console.WriteLine("{0} can fly in air nicely.", BirdName);
+        }
+
+    }
+
+    public class NonFlyingBird : CorrectApproach_Bird
+    {
+        public virtual void NonFly()
+        {
+            Console.WriteLine("{0} can not fly in air.", BirdName);
+        }
+
+    }
+
+    //public class Parrots : FlyingBird
+    //{
+    //    public override void Fly()
+    //    {
+    //        Console.WriteLine("{0} can fly in air nicely.", BirdName);
+    //    }
+    //}
+
+    //public class Orchids : NonFlyingBird
+    //{
+    //    public override void NonFly()
+    //    {
+    //        Console.WriteLine("{0} can not fly in air.", BirdName);
+    //    }
+    //}
+}
